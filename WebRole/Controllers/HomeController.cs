4930311538cb7774732fa754e30267ec09b98ab9
Feodor Fitsner <feodor@fitsner.com>
2014-03-20 21:00:50 -0700

    Initial check-in of Azure app

---
+++ WebRole/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            ViewBag.AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            return View();
+        }
+    }
+}
