c5405567f5e09018cffe9b0866784779adf25335
Feodor Fitsner <feodor@fitsner.com>
2014-03-21 20:19:43 -0700

    Tests added

---
+++ WebRole.Tests/MyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebRole.Tests
+{
+    public class MyTests
+    {
+        [Fact]
+        public void TestA()
+        {
+            Assert.Equal(2, 2);
+        }
+
+        [Fact]
+        public void TestB()
+        {
+            Assert.Equal(8, 8);
+        }
+    }
+}
